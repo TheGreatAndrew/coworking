e531a13b8cf0351fa2390d9601fc4067eeb03228
andrew <andrewvu0203@gmail.com>
2022-01-02 14:12:51 -0600

    add user through invite (frontend)

--- frontend/src/components/Main/ChatTopBar/index.tsx
+++ frontend/src/components/Main/ChatTopBar/index.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
+import { useSelector } from "react-redux";
 import { InputBase, IconButton } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
+import axios from "axios";
+
 
 // Local Imports
 import styles from "./styles.module.scss";
@@ -10,25 +13,69 @@ type Props = {
   menuClick: () => void;
 };
 
-const addUserToGroup = async () => {
-
-  // put middleware, and change joinGroupHandler() -> test if joinGroup works 
-  // store form input -> useState() 
-
-
-  // get token for Bearer 
-  // get gid
-  // get uid from form 
+type GroupData = {
+  _id: string;
+};
 
 
-  // send axios 
-
-
-
- 
+interface IRootState {
+  auth: {
+    id: string | null;
+    username: string | null;
+    token: string | null;
+  };
+  app: {
+    currentGroup: GroupData;
+  };
 }
 
 const TopBar: React.FC<Props> = (props) => {
+  const userData = useSelector((state: IRootState) => state.auth);
+  const {
+    currentGroup
+  } = useSelector((state: IRootState) => state.app);
+  const [addUserValue, setAddUserValue] = useState('');
+
+
+  // TODO snackbar 
+  const addUserToGroup = async () => {  
+    const { token } = userData;
+
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+    }
+
+    const bodyParameters = {
+      uid : addUserValue
+    }
+
+    console.log("test abc " + addUserValue)
+
+    // axios
+    let response;
+    try {
+      response = await axios.post(
+        `${
+          process.env.REACT_APP_MY_HEROKU_BACKEND_URL ||
+          process.env.REACT_APP_SERVER_URL
+        }/groups/invite/${currentGroup._id}`,
+        bodyParameters,
+        config
+      );
+    } catch (error) {
+      console.log("[ERROR][GROUPS][INVITE USERS]: ", error);
+      return;
+    }
+    if (!response) return;
+
+    
+
+  }
+
+  const addUserHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setAddUserValue(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
 
@@ -42,6 +89,8 @@ const TopBar: React.FC<Props> = (props) => {
               <InputBase
                 className={styles.textinput}
                 placeholder="Type the user id"
+                onChange={e => addUserHandler(e)}
+                value={addUserValue}
               />
             </div>
             <Button className={styles.addButton} onClick={addUserToGroup}>Add</Button>
