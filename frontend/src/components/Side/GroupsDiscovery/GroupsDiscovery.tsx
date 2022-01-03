627d34eda48cc68f83a65b351bc9cb552fcb89dd
andrew <andrewvu0203@gmail.com>
2022-01-03 16:34:41 -0600

    private group won't be seen

--- frontend/src/components/Side/GroupsDiscovery/GroupsDiscovery.tsx
+++ frontend/src/components/Side/GroupsDiscovery/GroupsDiscovery.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // context menu
 import {
@@ -14,11 +14,11 @@ import "@szhsin/react-menu/dist/index.css";
 // Local Imports
 import styles from "./styles.module.scss";
 import { boolean, string } from "yup/lib/locale";
-import CustomButton from '../../Shared/CustomButton';
+import CustomButton from "../../Shared/CustomButton";
 
 type PropsGroup = {
   title: string;
-  description : string;
+  description: string;
   key?: string;
   tag?: string;
   _id: string;
@@ -39,40 +39,69 @@ const Group: React.FC<PropsGroup> = (props) => {
         toggleMenu(true);
       }}
     >
-      <div
-        className={styles.group}
-      >
+      <div className={styles.group}>
         <span className={styles.tag}>{props.tag}</span>
         <p className={styles.title}>{props.title}</p>
         <span className={styles.tag}></span>
-        <p className={styles.title}>{"[description : "}{props.description}{"]"}</p>
+        <p className={styles.title}>
+          {"[description : "}
+          {props.description}
+          {"]"}
+        </p>
         <span className={styles.tag}></span>
-        <CustomButton onClick={() => props.openGroupClick(props._id)} isPurple title="Join" small />
-
+        <CustomButton
+          onClick={() => props.openGroupClick(props._id)}
+          isPurple
+          title="Join"
+          small
+        />
       </div>
       <ControlledMenu
         {...menuProps}
         anchorPoint={anchorPoint}
         onClose={() => toggleMenu(false)}
       >
-        <MenuItem onClick={() => props.leaveGroupClick(props._id, props._id)}>Leave Group</MenuItem>
-
+        <MenuItem onClick={() => props.leaveGroupClick(props._id, props._id)}>
+          Leave Group
+        </MenuItem>
       </ControlledMenu>
     </div>
   );
 };
 
 type PropsGroups = {
-  groups: PropsGroup[];
+  groups: GroupData[];
   openGroupClick: (id: string) => void;
   leaveGroupClick: (id: string, uid: string) => void;
 };
 
+type GroupData = {
+  _id: string;
+  title: string;
+  description: string;
+  owner: string;
+  isPrivate: boolean;
+  openGroupClick: () => void;
+  leaveGroupClick: () => void;
+};
+
 const GroupsDiscovery: React.FC<PropsGroups> = (props) => {
+  const [allGroups, setAllGroups] = useState(props.groups);
+
+
+  const filterOnlyPublicGroup = (allGroups: GroupData[]) => {
+    allGroups = allGroups.filter((group) => group.isPrivate == false);
+    return allGroups;
+  };
+
+  useEffect(() => { 
+    setAllGroups(filterOnlyPublicGroup(allGroups))
+  }, [])
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        {props.groups.map((group) => (
+        {allGroups.map((group) => (
           <Group
             _id={group._id}
             key={group._id}
@@ -81,7 +110,6 @@ const GroupsDiscovery: React.FC<PropsGroups> = (props) => {
             tag={`${group.title[0]}${group.title[1]}`.toUpperCase()}
             openGroupClick={(id) => props.openGroupClick(id)}
             leaveGroupClick={(id) => props.leaveGroupClick(id, id)}
-            
           />
         ))}
       </div>
