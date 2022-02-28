3766931c3b88f66ff407ae69dcf39c0b28cf03b0
andrew <andrewvu0203@gmail.com>
2022-02-28 00:55:20 -0600

    basic agile scrum board

---
+++ frontend/src/components/AgileBoard/Col.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./index.css";
+
+const Col = ({ isOver, children }) => {
+    const className = isOver ? " highlight-region" : "";
+
+    return (
+        <div className={`col${className}`}>
+            {children}
+        </div>
+    );
+};
+
+export default Col;
---
+++ frontend/src/components/AgileBoard/Window.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Modal from "react-modal";
+import styles from "./index.css";
+
+
+Modal.setAppElement("#root");
+
+const Window = ({ show, onClose, item }) => {
+
+    return (
+        <Modal
+            appElement={document.getElementById('app')}
+            isOpen={show}
+            onRequestClose={onClose}
+            className={"modal"}
+            overlayClassName={"overlay"}
+        >
+            <div className={"close-btn-ctn"}>
+                <h1 style={{ flex: "1 90%" }}>{item.title}</h1>
+                <button className="close-btn" onClick={onClose}>X</button>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <p>{item.content}</p>
+                <h2>Status</h2>
+                <p>{item.icon} {`${item.status.charAt(0).toUpperCase()}${item.status.slice(1)}`}</p>
+            </div>
+        </Modal>
+    );
+};
+
+export default Window;
